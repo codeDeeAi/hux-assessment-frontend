28f2bc95fb7e0488cdfe0c83a65221b0b6fc81a0
Adeola Bada <dunsin.bada@gmail.com>
2024-08-21 07:43:12 +0100

    [INIT] iniatialize project structure with dependencies and tailwind

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import AppHeader from '@/components/layouts/AppHeader.vue'
+</script>
+
+<template>
+  <main class="!w-screen !h-screen bg-white">
+    <AppHeader />
+    <RouterView class="!h-full overflow-auto" />
+  </main>
+</template>
+
+<style scoped></style>
